10eebca9bcd3e1e88030d724d7f9a1e9d210b47c
patoxo <patricio.cr95@protonmail.com>
2022-03-31 11:55:29 -0400

    updates

---
+++ src/Main.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        }
+
+       
+    }
+
+    increment = ()=>{
+        this.setState({number: this.state.number+1})
+    }
+
+    decrement = ()=>{
+        this.setState({number: this.state.number-1})
+    }
+
+    neutral = ()=>{
+        this.setState({number: 0})
+    }
+
+    render() {
+        return (
+            <div className='row justify-content-center'>
+                <div className='col-md-5' style={{ marginTop: '150px', border: '5px solid black ' }}>
+                    <h1>REACT COUNTER</h1>
+                    <h1>{this.state.number}</h1>
+                    <button className='btn btn-success' onClick={this.increment}>Increment</button>
+                    <button className='btn btn-danger' onClick={this.decrement}>Decrement</button>
+                    <button className='btn btn-primary' onClick={this.neutral}>Neutral</button>
+
+
+                </div>
+
+
+            </div>
+
+
+        );
+    }
+}
+
+export default Main;
